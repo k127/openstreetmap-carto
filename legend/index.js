de722dfc641acebcc9641bd04ef3111df3f66cda
k127 <k127@gmx.de>
2016-07-12 16:32:43 +0200

    Refine map feature json parsing structure

--- legend/index.js
+++ legend/index.js
@@ -1,6 +1,18 @@
 
-var mapFeatures = require('./map_features');
+var config = {
+	gridOffsetLat: 0,  // The latitude where you want to place the top of the legend
+	gridOffsetLon: 0   // The longitude where you want to place the left of the legend
+};
 
-for (var category in mapFeatures.categories) {
-	console.log(category);
+var builder = require('xmlbuilder');
+var mapFeatures = require('./map_features');
+var osmXmlTemplate = "";  // TODO replace string with some XML DOM
+
+for (var i in mapFeatures.categories) {
+	var category = mapFeatures.categories[i];
+	console.log(". category:", category.name);  // TODO only if verbose
+	for (var j in category.sections) {
+		var section = category.sections[j];
+		console.log(".. section:", section.name);  // TODO only if verbose
+	}
 }
